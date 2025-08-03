83be844cde18964ac748aefac201821ad208647f
evan-baron <egbaron@gmail.com>
2025-08-03 10:19:27 -0600

    summary complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,17 +16,35 @@ export default function Home() {
 			<main className={styles.main}>
 				<Header />
 				<Hero />
-				<div className={styles['about-wrapper']}>
-					<section id='about' className={styles.about}>
-						<div className={styles['image-background']}></div>
-						<Image
-							src='/data-extraction-cuate.svg'
-							alt='Data Extraction Illustration'
-							className={styles['data-image']}
-							width={600}
-							height={600}
-							priority={true}
-						/>
+				<div className={styles['summary-wrapper']}>
+					<section id='about' className={styles.summary}>
+						<div className={styles.wrapper}>
+							<div className={styles['image-background']}></div>
+						</div>
+						<div className={styles['image-wrapper']}>
+							<Image
+								src='/data-extraction-cuate.svg'
+								alt='Data Extraction Illustration'
+								className={styles['data-image']}
+								width={600}
+								height={600}
+								priority={true}
+							/>
+						</div>
+						<div className={styles['text-wrapper']}>
+							<h2 className={styles['summary-highlight']}>
+								Growth shouldn&#39;t feel like guesswork.
+							</h2>
+							<p className={styles['summary-text']}>
+								If you&#39;re stuck, circling the same challenges, or unsure
+								what&#39;s next, it&#39;s time to break that cycle. We help
+								companies move forward with purpose through design, product
+								strategy, and creative problem-solving.
+							</p>
+							<p className={styles['summary-text']}>
+								It&#39;s time to rewrite your narrative.
+							</p>
+						</div>
 					</section>
 				</div>
 				<section id='services' className={styles.services}>
