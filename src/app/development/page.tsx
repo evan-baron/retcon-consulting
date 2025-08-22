3c55660ea110b70abbd799db24fe62f51ada5b22
evan-baron <egbaron@gmail.com>
2025-08-21 22:05:09 -0600

    finished why choose custom section

--- src/app/development/page.tsx
+++ src/app/development/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 // Library imports
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
 
 // Hooks imports
 
@@ -18,11 +18,77 @@ import Tile from './developmentTile/Tile';
 // Data imports
 import { ReasonTiles } from '@/lib/data/why-custom';
 
+interface ReasonRefs {
+	index: number;
+	visible: boolean;
+}
+
 const Development = () => {
 	const [showMore, setShowMore] = useState(false);
 	const [isAnimating, setIsAnimating] = useState(false);
 	const visibleReasons = ReasonTiles.slice(0, 6);
 	const hiddenReasons = ReasonTiles.slice(6);
+
+	const tilesRef = useRef<HTMLDivElement | null>(null);
+	const reasonRefs = useMemo(
+		() => visibleReasons.map(() => React.createRef<HTMLDivElement>()),
+		[]
+	);
+
+	const [reasonRefsVisible, setReasonRefsVisible] = useState<ReasonRefs[]>(
+		reasonRefs.map((_, index) => ({
+			index,
+			visible: false,
+		}))
+	);
+
+	useEffect(() => {
+		const observer = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				if (entry.intersectionRatio > 0) {
+					const index = reasonRefs.findIndex(
+						(ref) => ref.current === entry.target
+					);
+					setReasonRefsVisible((prev) =>
+						prev.map((item) =>
+							item.index === index
+								? { ...item, visible: entry.isIntersecting }
+								: item
+						)
+					);
+				}
+			});
+		});
+
+		reasonRefs.forEach((ref) => {
+			if (ref.current) {
+				observer.observe(ref.current);
+			}
+		});
+
+		return () => {
+			observer.disconnect();
+		};
+	}, []);
+
+	// Resets the animations for the text boxes
+	useEffect(() => {
+		const handleScroll = () => {
+			if (!tilesRef.current) return;
+
+			const rect = tilesRef.current.getBoundingClientRect();
+
+			if (rect.top > window.innerHeight) {
+				// Timeline is below the viewport, reset visibility
+				setReasonRefsVisible(
+					reasonRefsVisible.map((item) => ({ ...item, visible: false }))
+				);
+			}
+		};
+
+		window.addEventListener('scroll', handleScroll);
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, [reasonRefsVisible]);
 
 	return (
 		<>
@@ -47,9 +113,17 @@ const Development = () => {
 				</section>
 				<section className={styles.custom}>
 					<h3>Why Choose Custom?</h3>
-					<div className={styles.tiles}>
+					<div className={styles.tiles} ref={tilesRef}>
 						{visibleReasons.map((reason, index) => (
-							<Tile key={index} {...reason} />
+							<div
+								key={index}
+								ref={reasonRefs[index]}
+								className={`${styles.tile} ${
+									reasonRefsVisible[index]?.visible ? styles.visible : ''
+								}`}
+							>
+								<Tile {...reason} />
+							</div>
 						))}
 					</div>
 					{showMore && (
@@ -75,11 +149,11 @@ const Development = () => {
 								setTimeout(() => {
 									setShowMore(false);
 									setIsAnimating(false);
-								}, 1000);
+								}, 350);
 							}
 						}}
 					>
-						{showMore ? 'Show Less' : 'See More Reasons'}
+						{showMore ? 'Show Less' : 'View More Reasons'}
 					</button>
 				</section>
 			</div>
