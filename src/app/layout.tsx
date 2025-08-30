0a035bbe03294f42dec17a4af5fcbbeb9c7f2c30
evan-baron <egbaron@gmail.com>
2025-08-29 21:42:04 -0600

    using google imports and no preloading fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata, Viewport } from 'next';
-// import { IBM_Plex_Mono, Fredericka_the_Great, Roboto } from 'next/font/google';
+import { IBM_Plex_Mono, Fredericka_the_Great, Roboto } from 'next/font/google';
 import './reset.css';
 import './globals.scss';
 
@@ -11,23 +11,23 @@ import Footer from './components/footer/Footer';
 // Context
 import { ContextProvider } from './context/AppContext';
 
-// const ibmPlexMono = IBM_Plex_Mono({
-// 	variable: '--font-ibm-plex-mono',
-// 	weight: ['400', '500', '600', '700'],
-// 	subsets: ['latin'],
-// });
+const ibmPlexMono = IBM_Plex_Mono({
+	variable: '--font-ibm-plex-mono',
+	weight: ['400', '500', '600', '700'],
+	subsets: ['latin'],
+});
 
-// const frederickaTheGreat = Fredericka_the_Great({
-// 	variable: '--font-fredericka-the-great',
-// 	weight: '400',
-// 	subsets: ['latin'],
-// });
+const frederickaTheGreat = Fredericka_the_Great({
+	variable: '--font-fredericka-the-great',
+	weight: '400',
+	subsets: ['latin'],
+});
 
-// const roboto = Roboto({
-// 	variable: '--font-roboto',
-// 	weight: ['400', '500', '700'],
-// 	subsets: ['latin'],
-// });
+const roboto = Roboto({
+	variable: '--font-roboto',
+	weight: ['400', '500', '700'],
+	subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
 	metadataBase: new URL('https://www.retconconsulting.com'),
@@ -186,9 +186,13 @@ export default function RootLayout({
 						__html: JSON.stringify(structuredData),
 					}}
 				/>
+				<link
+					href='https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap'
+					rel='stylesheet'
+				></link>
 			</head>
 			<body
-			// className={`${ibmPlexMono.variable} ${roboto.variable} ${frederickaTheGreat.variable}`}
+				className={`${ibmPlexMono.variable} ${roboto.variable} ${frederickaTheGreat.variable}`}
 			>
 				<ContextProvider>
 					<BackgroundEffect />
